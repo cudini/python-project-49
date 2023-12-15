82e1d33f64e222c97c3e6282f40d3da367e84984
Рыжов Дмитрий <main@cudini.ru>
2023-12-15 12:21:33 +0300

    entry point refactor

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-
-def greets():
-    print('Welcome to the Brain Games!')
+from brain_games.cli import welcome_user
 
 
 def main():
-    greets()
+    welcome_user()
 
 
 if __name__ == '__main__':
