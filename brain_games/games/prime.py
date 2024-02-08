5134ec2e1931212131705dbb339ac54963c36c21
Рыжов Дмитрий <main@cudini.ru>
2024-02-08 12:42:05 +0300

    refactor prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,23 +7,20 @@ MIN_NUMBER = 1
 MAX_NUMBER = 50
 
 
-def get_random_number():
-    return randint(MIN_NUMBER, MAX_NUMBER)
-
-
-def is_prime(random_number):
-    if random_number < 2 & 1:
+def is_prime(number):
+    if number < 2 and number % 2 == 0:
         return False
-    for devisor in range(2, int(sqrt(get_random_number)) + 1):
-        if random_number % devisor == 0:
+    for devisor in range(2, int(sqrt(number)) + 1):
+        if number % devisor == 0:
             return False
         return True
 
 
-def check_answer():
-    if is_prime(get_random_number):
+def get_game_data():
+    number = randint(MIN_NUMBER, MAX_NUMBER)
+    if is_prime(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    name_of_game = prime_number
-    return name_of_game, correct_answer
+    game_question = number
+    return game_question, correct_answer
