7a62d5930c76700f13f40d650ee74f33f53db2d9
Рыжов Дмитрий <main@cudini.ru>
2024-02-09 15:29:46 +0300

    refactor even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,19 +4,10 @@ from random import randint
 
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-MIN_NUMBER = 1
-MAX_NUMBER = 100
-
-
-def is_even(random_number: int) -> bool:
-    return random_number % 2 == 0
-
 
 def get_game_state():
-    number = randint(MIN_NUMBER, MAX_NUMBER)
-    if is_even(number):
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-    game_question = number
-    return game_question, correct_answer
+
+    number = randint(0, 100)
+    correct_answer = 'no' if number % 2 else 'yes'
+
+    return number, correct_answer
