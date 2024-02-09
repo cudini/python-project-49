7736460c74f321bd4e2553b7305d678464212f65
Рыжов Дмитрий <main@cudini.ru>
2024-02-09 09:13:29 +0300

    refactor start_game function

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,25 +1,30 @@
+'''Main game logic interface'''
 import prompt
 
 
-GAME_SCORE = 3
+GAME_CONTINUE = 3
 
 
 def start_game(game):
+
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
     print(game.DESCRIPTION)
     game_try = 0
-    while game_try < GAME_SCORE:
-        question, correct_answer = game.get_data()
-        print(f'Question: {question}')
-        answer = prompt.string('Your answer: ')
-        if answer == correct_answer:
+
+    while game_try < GAME_CONTINUE:
+        game_question, correct_answer = game.get_game_state()
+        print(f'Question: {game_question}')
+        user_answer = prompt.string('Your answer: ')
+        if user_answer == correct_answer:
             print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(."
+            print(f"'{user_answer}' is wrong answer ;(."
                   f"Correct answer was '{correct_answer}'.")
-            print(f'Let\'s try again, {name}!')
+            print(f"Let 's try again, {user_name}!")
             return
+
         game_try += 1
-    print(f'Congratulanions, {name}!')
+
+    print(f'Congratulanions, {user_name}!')
