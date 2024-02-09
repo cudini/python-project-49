7e887b3ae048fafb31b6bf2be5ecdd140d1f6129
Рыжов Дмитрий <main@cudini.ru>
2024-02-09 12:15:37 +0300

    refactor concrete logic to library import gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,17 +1,12 @@
 '''Great common divisor game concrete logic'''
+from math import gcd
 from random import randint
 
 
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 LOWER_LIMIT = 2
-UPPER_LIMIT = 200
-
-
-def get_gcd(number_1: int, number_2: int) -> int:
-    while number_2:
-        number_1, number_2 = number_2, number_1 % number_2
-        return number_1
+UPPER_LIMIT = 50
 
 
 def get_game_state():
@@ -19,6 +14,6 @@ def get_game_state():
     second_number = randint(LOWER_LIMIT, UPPER_LIMIT)
 
     game_question = f'{first_number} {second_number}'
-    correct_answer = get_gcd(first_number, second_number)
+    correct_answer = str(gcd(first_number, second_number))
 
     return game_question, correct_answer
