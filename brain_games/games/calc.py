24ae246924132416f147e4f1e2174de02c6ed47e
Рыжов Дмитрий <main@cudini.ru>
2024-02-09 15:02:26 +0300

    refactor calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -14,8 +14,6 @@ def get_game_state():
     operand_1 = randint(MIN_NUMBER, MAX_NUMBER)
     operand_2 = randint(MIN_NUMBER, MAX_NUMBER)
     operation = choice(list(OPERATORS.keys()))
-
     game_question = f'{operand_1} {operation} {operand_2}'
     correct_answer = str(OPERATORS[operation](operand_1, operand_2))
-
     return game_question, correct_answer
