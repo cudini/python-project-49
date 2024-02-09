57f9077080a44a718b204834f340918949b11c08
Рыжов Дмитрий <main@cudini.ru>
2024-02-09 11:16:59 +0300

    realize gcd algorythm

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,24 @@
+'''Great common divisor game concrete logic'''
+from random import randint
+
+
+LOWER_LIMIT = 2
+UPPER_LIMIT = 200
+
+
+def get_gcd(number_1: int, number_2: int) -> int:
+    while number_2 != 0:
+        (number_1, number_2) = (number_2, number_1 % number_2)
+        if number_1 >= 0:
+            return number_1
+        return (-number_1)
+
+
+def get_game_state():
+    first_number = randint(LOWER_LIMIT, UPPER_LIMIT)
+    second_number = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+    game_question = f'{first_number} {second_number}'
+    correct_answer = get_gcd(first_number, second_number)
+
+    return game_question, correct_answer
