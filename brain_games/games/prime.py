56bedb666251272a7f9a51081c6a4dd2f23427c5
Рыжов Дмитрий <main@cudini.ru>
2024-02-09 09:11:20 +0300

    refactor function

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,13 +1,15 @@
+'''Prime game concrete logic'''
 from random import randint
 from math import sqrt
 
 
 DESCRIPTION = 'Answer "yes" if given is prime. Otherwise answer "no".'
+
 MIN_NUMBER = 1
 MAX_NUMBER = 50
 
 
-def is_prime(number):
+def is_prime(number: int) -> bool:
     if number < 2 and number % 2 == 0:
         return False
     for devisor in range(2, int(sqrt(number)) + 1):
@@ -16,7 +18,7 @@ def is_prime(number):
         return True
 
 
-def get_game_data():
+def get_game_state():
     number = randint(MIN_NUMBER, MAX_NUMBER)
     if is_prime(number):
         correct_answer = 'yes'
