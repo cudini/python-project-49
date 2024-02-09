93f424e472f7ebc7288b15403b2e7661e2fffb0d
Рыжов Дмитрий <main@cudini.ru>
2024-02-09 11:17:36 +0300

    refactor structure of code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -13,16 +13,16 @@ def is_prime(number: int) -> bool:
     if number < 2 and number % 2 == 0:
         return False
     for devisor in range(2, int(sqrt(number)) + 1):
-        if number % devisor == 0:
-            return False
-        return True
+        return number % devisor == 0
 
 
 def get_game_state():
     number = randint(MIN_NUMBER, MAX_NUMBER)
+
     if is_prime(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
     game_question = number
+
     return game_question, correct_answer
