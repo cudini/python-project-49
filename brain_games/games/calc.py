4b80d086daebaef5012d1a361988380c3c49e669
Рыжов Дмитрий <main@cudini.ru>
2024-02-14 14:22:50 +0300

    refactor from dict to list of tuples

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,16 +7,16 @@ DESCRIPTION = 'What is the result of the expression?'
 
 MIN_NUMBER = 0
 MAX_NUMBER = 50
-OPERATORS = {'+': add, '-': sub, '*': mul}
+OPERATORS = [('+', add), ('-', sub), ('*', mul)]
 
 
-def get_game_state():
+def get_quest_and_answer():
 
     operand_1 = randint(MIN_NUMBER, MAX_NUMBER)
     operand_2 = randint(MIN_NUMBER, MAX_NUMBER)
-    operation = choice(list(OPERATORS.keys()))
+    description, operation = choice(OPERATORS)
 
-    game_question = f'{operand_1} {operation} {operand_2}'
-    correct_answer = str(OPERATORS[operation](operand_1, operand_2))
+    game_question = f'{operand_1} {description} {operand_2}'
+    correct_answer = str(operation(operand_1, operand_2))
 
     return game_question, correct_answer
