1410d7dbc4de34064128243994066b762cb78b65
Рыжов Дмитрий <main@cudini.ru>
2024-02-14 14:23:54 +0300

    refactor logic to one function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,27 +10,19 @@ UPPER_BOUND = 50
 DELTA_LIMIT = 10
 
 
-def get_game_state():
+def get_quest_and_answer():
 
     delta = randint(LOWER_BOUND, DELTA_LIMIT)
     first_number = randint(LOWER_BOUND, UPPER_BOUND)
     missing_number = randint(0, PROGRESSION_LENGTH - 1)
-    progression = to_string(make_progression(first_number, delta))
+    last_number = first_number + (PROGRESSION_LENGTH * delta)
+
+    progression = []
+    for number in range(first_number, last_number, delta):
+        progression.append(str(number))
 
     correct_answer = progression[missing_number]
     progression[missing_number] = '..'
-    game_question = ' '.join(progression)
+    game_question = ' '.join(map(str, progression))
 
     return game_question, correct_answer
-
-
-def make_progression(first_number: int, delta: int) -> range:
-    last_number = first_number + (PROGRESSION_LENGTH * delta)
-    return range(first_number, last_number, delta)
-
-
-def to_string(progression) -> str:
-    result = []
-    for index in progression:
-        result.append(str(index))
-    return result
